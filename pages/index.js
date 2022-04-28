592b92414fb58a8050e848c656a92d2a21c25a9d
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-04-27 23:37:04 -0400

    Update Header

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Foodcu</h1>
     </div>
   );
 }
