3f883d37e59eb7584a0bd0b1316ff82e739deb3d
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-04-30 16:03:15 -0400

    Add layout components

---
+++ components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+function Layout({ children }) {
+  return (
+    <Fragment>
+      <Navbar />
+      {children}
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default Layout;
